6735108794f286b916a9a07c65cedba489ea45b7
Luis Rascão <luis.rascao@gmail.com>
2014-04-24 10:02:09 +0100

    second dataset

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-getdata-002-project
+# getdata-002-project
 ===================
 
-Project for Coursera's Getting and Cleaning data course
+## Project for Coursera's Getting and Cleaning data course
+
+run_analysis.R processes a set of data containing various sensor readings for various subjects performing several activites
+
+### Requirements:
+* data folder at the same level of run_analysis.R with the contents of (https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)
+
+
+### Run instructions
+* Rscript run_analysis.R
+
+### Description
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,40 @@
+##
+## 	* Merges the training and the test sets to create one data set.
+##	* Extracts only the measurements on the mean and standard deviation for each measurement. 
+##	* Uses descriptive activity names to name the activities in the data set
+##	* Appropriately labels the data set with descriptive activity names. 
+##	* Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+#
 
+## replaces activity ids with the corresponding activity labels obtained from activity_labels.txt
+##
+## Args:
+##	y: data frame with all activity ids
+##	activities: vector containing activity names
+##
+## Returns:
+##	data frame containing activity names
 set_activity_labels <- function(y, activities) {
-	# replaces activity ids with the corresponding activity labels obtained from activity_labels.txt
-	#
-	# Args:
-	#	y: data frame with all activity ids
-	#	activities: vector containing activity names
-	#
-	# Returns:
-	#	data frame containing activity names
 
 	lapply(y, function(x) {replace(x, TRUE, as.character(activities[x]))})
 }
 
+## builds the first tidy dataset required
+## 	1. merges the two datasets, training and test, for both X, y, and subject list
+##	2. adds a activity column factor
+##	3. builds a feature subset vector containing only the column names containing mean, Mean and std
+##	4, build a dataset filtered by the feature subset
+##	5. returns both the filtered and unfiltered datasets
+##
+## args:
+##		X_train_dataset
+##		X_test_dataset
+##		y_train_dataset
+##		y_test_dataset
+##		subjects_train
+##		subjects_test
+##		activity_labels - factor containing activity labels (WALKING, etc)
+##
 first_dataset <- function(X_train_dataset, X_test_dataset, 
 				      y_train_dataset, y_test_dataset,
 				      subjects_train, subjects_test,
@@ -38,10 +61,10 @@ first_dataset <- function(X_train_dataset, X_test_dataset,
 	dataset <- cbind(y_activity_labels, dataset)
 
 	# merge the two subject files
-	subjects <- rbind(subjects_train, subjects_test)$subject_id
+	subject <- rbind(subjects_train, subjects_test)$subject_id
 	
 	# add the subjects to the data table
-	dataset <- cbind(subjects, dataset)
+	dataset <- cbind(subject, dataset)
 
 	# now filter out the remaining features
 	filtered_dataset <- dataset[features_subset]
@@ -50,12 +73,16 @@ first_dataset <- function(X_train_dataset, X_test_dataset,
 	list("data" = dataset, "filtered_data" = filtered_dataset)
 }
 
+## builds the second tidy dataset required
+##
+## args:
+##	dataset1 - unfiltered dataset obtained from the first part of the assignment
+##
 second_dataset <- function(dataset1) {
 
 	# Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 	
-	
-	
+	aggregate(. ~ activity + subject, dataset1, mean)
 }
 
 # read the features file
